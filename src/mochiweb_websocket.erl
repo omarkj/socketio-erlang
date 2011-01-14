becd14a644ec5c959b1d70785ec3e89e46b92b90
Ómar Kjartan Yasin <omarkj@gmail.com>
2011-01-14 21:56:41 +0000

    Started work on a mochiweb websocket addon. It's works in the sense that mochiweb_websocket will establish a connecion to a websocket client.
    I've yet to abstract away the websocket stuff to make it nicer to work with.

---
+++ src/mochiweb_websocket.erl
@@ -0,0 +1,124 @@
+% Ported from Misultin
+
+-module (mochiweb_websocket).
+
+-export ([check/1, create_ws/3]).
+
+% External API
+check(Headers) ->
+	SupportedVersions = [{'draft-hixie', 76}, {'draft-hixie', 68}],
+	check_websockets(SupportedVersions, Headers).
+
+create_ws(Req, Version, Autoexit) ->
+	Ws = [
+		{version, Version}, {socket, Req:get(socket)}, {scheme, Req:get(scheme)},
+		{path, Req:get(path)}, {headers, Req:get(headers)}, {autoexit, Autoexit}
+	],% This should be a record, or I could change this to whatever Mochi uses by default
+		
+	Headers = proplists:get_value(headers, Ws),
+	Host = mochiweb_headers:get_value(host, Headers),
+	Origin = mochiweb_headers:get_value(origin, Headers),
+	Socket = proplists:get_value(socket, Ws),
+	
+	Handshake = create_handshake(
+		proplists:get_value(version, Ws),
+		Socket,
+		Headers,
+		proplists:get_value(path, Ws),
+		Origin,
+		Host),
+	
+	io:format("Handshake is ~p~n", [Handshake]),
+	
+	% Send the handshake
+	mochiweb_socket:send(Socket, Handshake),
+	io:format("SO, WS? ~p~n", Ws),
+	void.
+
+%% ------------
+%% Internal API
+%% ------------
+%% Start to check if WS is supported
+check_websockets([], _) -> false;
+
+check_websockets([Version|Rest], Headers) ->
+	case check_websocket(Version, Headers) of
+		false ->
+			check_websockets(Rest, Headers);
+		{true, _} ->
+			{true, Version}
+	end.
+
+%% WS Header check logic
+check_websocket({'draft-hixie', 76} = Version, Headers) ->
+	RequiredHeaders = [
+		{upgrade, "WebSocket"}, {connection, "Upgrade"}, {host, ignore}, {origin, ignore},
+		{'sec-websocket-key1', ignore}, {'sec-websocket-key2', ignore}
+	],
+	case loop_through_headers(Headers, RequiredHeaders) of
+		true ->
+			{true, Version};
+		_ ->
+			false
+	end;
+	
+check_websocket({'draft-hixie', 68} = Version, Headers) ->
+	RequiredHeaders = [
+		{upgrade, "WebSocket"}, {connection, "Upgrade"}, {host, ignore},
+		{origin, ignore}
+	],
+	case loop_through_headers(Headers, RequiredHeaders) of
+		true -> {true, Version};
+		_ -> false
+	end.
+
+loop_through_headers(Headers, RequiredHeaders) ->
+	lists:all(
+		fun({Key, Value}) ->
+			case mochiweb_headers:get_value(Key, Headers) of
+				HeaderValue ->
+					case Value of
+						ignore -> true; % Ignoring it
+						HeaderValue -> true; % Okay, this is a match
+						_ -> false % Returns false, these headers don't match 
+					end
+			end
+		end, RequiredHeaders).
+
+%% Create the handshake logic
+create_handshake({'draft-hixie', 76}, Socket, Headers, Path, Origin, Host) ->
+	SecKey1 = mochiweb_headers:get_value('sec-websocket-key1', Headers),
+	SecKey2 = mochiweb_headers:get_value('sec-websocket-key2', Headers),
+	mochiweb_socket:setopts(Socket, [{packet, raw}, {active, false}]),
+	Body = case mochiweb_socket:recv(Socket, 8, 30*1000) of
+		{ok, Bin} ->
+			Bin;
+		{error, timeout} ->
+			<<>>; % ERROR: Timeout
+		_ ->
+			<<>> % ERROR: Dunno
+	end,
+	["HTTP/1.1 101 WebSocket Protocol Handshake\r\n",
+		"Upgrade: WebSocket\r\n",
+		"Connection: Upgrade\r\n",
+		"Sec-WebSocket-Origin: ", Origin, "\r\n",
+		"Sec-WebSocket-Location: ws://", lists:concat([Host, Path]), "\r\n\r\n",
+		build_challenge({'draft-hixie', 76}, SecKey1, SecKey2, Body)
+	];
+create_handshake({'draft-hixie', 68}, _, _, Path, Origin, Host) ->
+	["HTTP/1.1 101 Web Socket Protocol Handshake\r\n",
+		"Upgrade: WebSocket\r\n",
+		"Connection: Upgrade\r\n",
+		"WebSocket-Origin: ", Origin , "\r\n",
+		"WebSocket-Location: ws://", lists:concat([Host, Path]), "\r\n\r\n"
+	].
+
+build_challenge({'draft-hixie', 76}, SecKey1, SecKey2, Body) ->
+	Ikey1 = [D || D <- SecKey1, $0 =< D, D =< $9],
+	Ikey2 = [D || D <- SecKey2, $0 =< D, D =< $9],
+	Blank1 = length([D || D <- SecKey1, D =:= 32]),
+	Blank2 = length([D || D <- SecKey2, D =:= 32]),
+	Part1 = list_to_integer(Ikey1) div Blank1,
+	Part2 = list_to_integer(Ikey2) div Blank2,
+	Ckey = <<Part1:4/big-unsigned-integer-unit:8, Part2:4/big-unsigned-integer-unit:8, Body/binary>>,
+	erlang:md5(Ckey).
---
+++ src/test.erl
@@ -0,0 +1,45 @@
+-module (test).
+
+-export([ start/1, loop/1
+        ]).
+
+%% internal export (so hibernate can reach it)
+-export([ resume/3
+        ]).
+
+-define(LOOP, {?MODULE, loop}).
+
+start(Port) ->
+		Options = [{port, Port}],
+    mochiweb_http:start([{name, ?MODULE}, {loop, ?LOOP} | Options]).
+
+loop(Req) ->
+	{Bool, Version} = mochiweb_websocket:check(Req:get(headers)),
+	io:format("Is this websockets? ~p~n", [Bool]),
+   case Bool of
+   	true ->
+			Ws = mochiweb_websocket:create_ws(Req, Version, true),
+			io:format("WS is ~p~n", [Ws]);
+		false ->
+			io:format("FUCK OFF")
+   end,
+   ok.
+
+%% this is the function that's called when a message arrives.
+resume(Req, RestOfPath, Reentry) ->
+    receive
+        Msg ->
+            Text = io_lib:format("wake up message: ~p~nrest of path: ~p", [Msg, RestOfPath]),
+            ok(Req, Text)
+    end,
+
+    %% if we didn't call @Reentry@ here then the function would finish and the
+    %% process would exit.  calling @Reentry@ takes care of returning control
+    %% to @mochiweb_http@
+    io:format("reentering loop via continuation in ~p~n", [Req:get(path)]),
+    Reentry(Req).
+
+ok(Req, Response) ->
+    Req:ok({_ContentType = "text/plain",
+            _Headers = [],
+            Response}).
